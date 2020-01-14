3534aac6e63cc2bc8cdcdbc9252dd0a94fa1cc81
makoto <makoto0826@gmail.com>
2020-01-14 20:09:16 +0900

    修正

--- functions/src/api/addTimeRecord.ts
+++ functions/src/api/addTimeRecord.ts
@@ -12,88 +12,96 @@ moment.tz.setDefault('Asia/Tokyo');
 const firestore = admin.firestore();
 
 export default functions
-    .region(TOKYO_REGION)
-    .runWith(RUNTIME_OPTIONS)
-    .https.onRequest(async (req, res) => {
-        const apiKey = req.header(X_API_KEY) as string
+    .region(TOKYO_REGION)
+    .runWith(RUNTIME_OPTIONS)
+    .https.onRequest(async (req, res) => {
+        const apiKey = req.header(X_API_KEY) as string
 
-        try {
-            if (!await checkApiKey(apiKey)) {
-                res.status(401).send({ message: 'unauthorized' });
-                return;
-            }
+        try {
+            if (!await checkApiKey(apiKey)) {
+                res.status(401).send({ message: 'unauthorized' });
+                return;
+            }
 
-            const request = req.body as AddTimeRecordRequest
+            const request = req.body as AddTimeRecordRequest
 
-            const userSnapshot = await firestore.collection(FirestoreCollectionNames.USERS)
-                .where('userId', '==', request.userId)
-                .where('cards', 'array-contains', request.cardId)
-                .get();
+            const userSnapshot = await firestore.collection(FirestoreCollectionNames.USERS)
+                .where('userId', '==', request.userId)
+                .where('cards', 'array-contains', request.cardId)
+                .get();
 
-            if (userSnapshot.empty) {
-                console.error('user not found', request.localTimeRecordId);
-                res.status(400).send({ message: 'user not found' });
-                return;
-            }
+            if (userSnapshot.empty) {
+                console.error('user not found', request.localTimeRecordId);
+                res.status(400).send({ message: 'user not found' });
+                return;
+            }
 
-            const userData = userSnapshot.docs[0].data() as UserData;
-            let status = TimeRecordStatus.OK;
+            const kingOfTimeSnapshot = await firestore.collection(FirestoreCollectionNames.INFORMATION)
+                .doc(FirestoreDocumentNames.KING_OF_TIME)
+                .get();
 
-            await firestore.runTransaction(async (tx) => {
-                const timeRecordRef = firestore.collection(FirestoreCollectionNames.TIME_RECORDS).doc();
+            if (!kingOfTimeSnapshot.exists) {
+                console.error('king of time toekn not found');
+                res.status(400).send({ message: 'king of time toekn not found' });
+                return;
+            }
 
-                const timeRecordSnapshot = await firestore.collection(FirestoreCollectionNames.TIME_RECORDS)
-                    .where('localTimeRecordId', '==', request.localTimeRecordId)
-                    .get();
+            const kingOfTimeData = kingOfTimeSnapshot.data() as KingOfTimeData;
 
-                if (!timeRecordSnapshot.empty) {
-                    console.error('duplicate localTimeRecordId', request.localTimeRecordId);
-                    res.status(400).send({ message: 'duplicate localTimeRecordId' });
-                    return;
-                }
+            const userData = userSnapshot.docs[0].data() as UserData;
+            let status = TimeRecordStatus.OK;
 
-                const snapshot = await firestore.collection(FirestoreCollectionNames.INFORMATION)
-                    .doc(FirestoreDocumentNames.KING_OF_TIME)
-                    .get();
+            await firestore.runTransaction(async (tx) => {
+                const timeRecordQuery = firestore.collection(FirestoreCollectionNames.TIME_RECORDS)
+                    .where('localTimeRecordId', '==', request.localTimeRecordId);
 
-                const kingOfTimeData = snapshot.data() as KingOfTimeData;
-                const api = new KingOfTimeApi(new KingOfTimeApiOptions(kingOfTimeData.token));
+                const timeRecordSnapshot = await tx.get(timeRecordQuery);
 
-                const registerdAt = moment(request.registeredAt);
+                if (!timeRecordSnapshot.empty) {
+                    console.error('duplicate localTimeRecordId', request.localTimeRecordId);
+                    res.status(400).send({ message: 'duplicate localTimeRecordId' });
+                    return;
+                }
 
-                const addDailyTimeRecordRequest = {
-                    employeeKey: userData.kingOfTimeId,
-                    date: registerdAt.format('YYYY-MM-DD'),
-                    time: registerdAt.format('YYYY-MM-DDTHH:mm:Z'),
-                    code: request.type
-                };
+                const api = new KingOfTimeApi(new KingOfTimeApiOptions(kingOfTimeData.token));
 
-                console.log(addDailyTimeRecordRequest);
+                const registerdAt = moment(request.registeredAt);
 
-                const result = await api.addDailyTimeRecord(addDailyTimeRecordRequest);
+                const addDailyTimeRecordRequest = {
+                    employeeKey: userData.kingOfTimeId,
+                    date: registerdAt.format('YYYY-MM-DD'),
+                    time: registerdAt.format('YYYY-MM-DDTHH:mm:00Z'),
+                    code: request.type
+                };
 
-                if (!result.ok) {
-                    status = TimeRecordStatus.API_ERROR;
-                    console.error(result.json);
-                }
+                console.log('addDailyTimeRecordRequest', addDailyTimeRecordRequest);
 
-                tx.create(timeRecordRef, {
-                    localTimeRecordId: request.localTimeRecordId,
-                    cardId: request.cardId,
-                    userId: request.userId,
-                    userName: request.userName,
-                    type: request.type,
-                    status: status,
-                    registeredAt: request.registeredAt,
-                    kingOfTimeId: userData.kingOfTimeId,
-                    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                    updatedAt: admin.firestore.FieldValue.serverTimestamp()
-                } as TimeRecordData);
-            });
+                const result = await api.addDailyTimeRecord(addDailyTimeRecordRequest);
 
-            res.send();
-        } catch (ex) {
-            console.error(ex);
-            res.status(500).send({ message: 'internal server error' });
-        }
-    });
+                if (!result.ok) {
+                    status = TimeRecordStatus.API_ERROR;
+                    console.error('error json', result.json);
+                }
+
+                const timeRecordRef = firestore.collection(FirestoreCollectionNames.TIME_RECORDS).doc();
+
+                tx.create(timeRecordRef, {
+                    localTimeRecordId: request.localTimeRecordId,
+                    cardId: request.cardId,
+                    userId: request.userId,
+                    userName: request.userName,
+                    type: request.type,
+                    status: status,
+                    registeredAt: request.registeredAt,
+                    kingOfTimeId: userData.kingOfTimeId,
+                    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+                    updatedAt: admin.firestore.FieldValue.serverTimestamp()
+                } as TimeRecordData);
+            });
+
+            res.send();
+        } catch (ex) {
+            console.error(ex);
+            res.status(500).send({ message: 'internal server error' });
+        }
+    });
--- functions/src/kingOfTime/model/timeRecordType.ts
+++ functions/src/kingOfTime/model/timeRecordType.ts
@@ -1,6 +1,6 @@
 export enum TimeRecordType {
     IN = '1',
     OUT = '2',
-    GO_OUT_IN = '7',
-    GO_OUT_OUT = '8'
+    GO_OUT = '7',
+    GO_OUT_BACK = '8'
 }
